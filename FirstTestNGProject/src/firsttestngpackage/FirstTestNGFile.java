92d8e72e3a0a83fe550e535bfc0fa204587a3306
Nuwan <nuwan41@gmail.com>
2018-04-27 12:58:51 -0500

    First Commit by Nuwan

---
+++ FirstTestNGProject/src/firsttestngpackage/FirstTestNGFile.java
@@ -0,0 +1,38 @@
+package firsttestngpackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirstTestNGFile {
+	WebDriver driver;
+	public String url="http://demo.guru99.com/test/newtours/";
+	public String ChromeDriverPath="C:\\Nuwan\\Software\\Selinium\\Chromedriver\\v2.37\\chromedriver.exe";
+	
+	@Test
+	public void verifyTitle() {
+	  	 
+	  String expectedTitle="Welcome: Mercury Tours";
+	  String actualTitle=driver.getTitle();	  
+	  Assert.assertEquals(actualTitle, expectedTitle);	  
+	  	  
+  }
+	
+	@BeforeTest
+	public void launchBrowser(){
+	 System.setProperty("webdriver.chrome.driver", ChromeDriverPath);
+	 driver=new ChromeDriver();		  
+	 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		  
+	 driver.get(url);		
+	}
+	
+	@AfterTest
+	public void closeBrowser(){
+		driver.close();		  
+	}
+}
